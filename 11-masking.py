ddb3298860d028fd75e692909d5170a1ee3874c0
Mukul <74971960+mk-mukul@users.noreply.github.com>
2022-07-11 08:00:42 +0530

    masking

---
+++ 11-masking.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import numpy as np
+
+# Masking focus in specefic part of image
+
+img = cv.imread('images/car.jpg')
+cv.imshow('Car', img)
+
+blank = np.zeros(img.shape[:2], dtype='uint8')
+cv.imshow("Blank", blank)
+
+mask = cv.circle(blank, (img.shape[1]//2, img.shape[0]//2), 100, 255, -1)
+cv.imshow("Mask", mask)
+
+masked = cv.bitwise_and(img, img, mask=mask)
+cv.imshow("Masked", masked)
+
+cv.waitKey(0)
+
+
+
